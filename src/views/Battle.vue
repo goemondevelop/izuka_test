aaba7c7b0a6348f542386ac5f3fd85d30204d2b3
goem <goemon.develop@gmail.com>
2020-08-13 09:00:27 +0900

    ルーティング設定

---
+++ src/views/Battle.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="map">
+    battle
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Battle',
+  data () {
+    return {
+      bar: 'foo',
+    }
+  },
+  computed: {
+    foo () {
+      return 'bar'
+    },
+  },
+  watch: {
+    bar: {
+      handler(val, old){
+        console.log(val)
+        console.log(old)
+      }, deep: true
+    }
+  },
+  methods: {
+    doSomething () {
+      console.log('do something')
+    }
+  },
+  components: {
+  },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Map.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="map">
+    map
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Map',
+  data () {
+    return {
+      bar: 'foo',
+    }
+  },
+  computed: {
+    foo () {
+      return 'bar'
+    },
+  },
+  watch: {
+    bar: {
+      handler(val, old){
+        console.log(val)
+        console.log(old)
+      }, deep: true
+    }
+  },
+  methods: {
+    doSomething () {
+      console.log('do something')
+    }
+  },
+  components: {
+  },
+}
+</script>
+
+<style scoped>
+</style>
